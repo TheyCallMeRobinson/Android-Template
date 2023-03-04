94ef6fa645c8cfafd4a055a093e0ef38499de511
Paul Robinson <alettertopaulrobinson0099@gmail.com>
2023-03-04 12:01:41 +0300

    Create domain models and lamp repository

---
+++ domain/src/main/java/cs/vsu/ru/domain/repository/LampRepository.kt
@@ -0,0 +1,8 @@
+package cs.vsu.ru.domain.repository
+
+import cs.vsu.ru.domain.model.BarcodeRequestEntity
+import cs.vsu.ru.domain.model.LampDataResponseEntity
+
+interface LampRepository {
+    fun getData(request: BarcodeRequestEntity): LampDataResponseEntity
+}
